fedce2405163b6536cf7f4f8ed181c645925b494
Tung_Nguyen <7319tnguyen040@gmail.com>
2016-11-14 13:55:46 -0800

    Improved graphics, added turrets, added projectiles, partially finished lasers, reorganized assets, lots more

---
+++ Assets/ChainsOfTheVoid/Scripts/VFX/ParticleRotationToParent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleRotationToParent : MonoBehaviour
+{
+    private ParticleSystem TheParticleSystem;
+
+    void Start ()
+    {
+        TheParticleSystem = GetComponent<ParticleSystem>();
+    }
+	
+	void Update ()
+    {
+        TheParticleSystem.startRotation3D = transform.root.rotation.eulerAngles * Mathf.Deg2Rad;
+    }
+}
